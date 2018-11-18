0384a6462fb6cce45cc0c5120f04dc6eff8fbc9b
Deepak <deepak.pokhrel@hotmail.com>
2018-11-18 10:58:55 -0600

    added ionic library

---
+++ farmstodoor.sql
@@ -0,0 +1,12 @@
+use oms;
+
+select * from category;
+select * from product;
+select * from details;
+select * from price;
+select * from Category_averageRatings;
+select * from image;
+
+show tables;
+
+
